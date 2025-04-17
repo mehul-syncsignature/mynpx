0a0745e322e0fa22600d131d6b80193a89abe81a
mehul-syncsignature <mehul.syncsignature@gmail.com>
2025-04-17 12:01:48 +0530

    fix: common type changes

---
+++ component-registry/components/types/index.ts
@@ -0,0 +1,42 @@
+interface Data {
+    heading: string;
+    description: string;
+    imageUrl: string;
+    imageAlt?: string;
+    buttonText: string;
+    communityButtonText?: string;
+    ctaButtonText?: string;
+}
+
+interface CommonConfig {
+    width: number;
+    height: number;
+    buttonStyle?: "rounded" | "square";
+    fontSize?: {
+        heading?: string;
+        description?: string;
+        button?: number;
+    };
+    backdropConfig?: {
+        backdropUrl?: string;
+        backdropPosition?: string;
+    }
+    textColor?: string;
+}
+
+// Match the existing BrandConfigState interface
+interface BrandConfig {
+    primaryColor: string;
+    secondaryColor: string;
+    highlightColor: string;
+    primaryFont: string;
+    secondaryFont: string;
+    highlightFont: string;
+}
+
+export interface CommonComponetProps {
+    data: Data;
+    commonConfig: CommonConfig;
+    brandConfig: BrandConfig;
+    className?: string;
+}
