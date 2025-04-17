46d3a10652f74f7b596eeb780e27cf11a61c7565
mehul-syncsignature <mehul.syncsignature@gmail.com>
2025-04-17 15:25:28 +0530

    fix: fixed the index.ts type file

--- component-registry/components/types/index.ts
+++ component-registry/components/types/index.ts
@@ -1,42 +1,43 @@
-interface Data {
-    heading: string;
-    description: string;
-    imageUrl: string;
-    imageAlt?: string;
-    buttonText: string;
-    communityButtonText?: string;
-    ctaButtonText?: string;
+export interface Data {
+  heading?: string;
+  subHeading?: string;
+  description?: string;
+  imageUrl?: string;
+  imageAlt?: string;
+  buttonText?: string;
+  communityButtonText?: string;
+  ctaButtonText?: string;
 }
 
-interface CommonConfig {
-    width: number;
-    height: number;
-    buttonStyle?: "rounded" | "square";
-    fontSize?: {
-        heading?: string;
-        description?: string;
-        button?: number;
-    };
-    backdropConfig?: {
-        backdropUrl?: string;
-        backdropPosition?: string;
-    }
-    textColor?: string;
+export interface CommonConfig {
+  width: number;
+  height: number;
+  buttonStyle?: "rounded" | "square";
+  fontSize?: {
+    heading?: string;
+    description?: string;
+    button?: number;
+  };
+  backdropConfig?: {
+    backdropUrl?: string;
+    backdropPosition?: string;
+  };
+  textColor?: string;
 }
 
 // Match the existing BrandConfigState interface
-interface BrandConfig {
-    primaryColor: string;
-    secondaryColor: string;
-    highlightColor: string;
-    primaryFont: string;
-    secondaryFont: string;
-    highlightFont: string;
+export interface BrandConfig {
+  primaryColor: string;
+  secondaryColor: string;
+  highlightColor: string;
+  primaryFont: string;
+  secondaryFont: string;
+  highlightFont: string;
 }
 
 export interface CommonComponetProps {
-    data: Data;
-    commonConfig: CommonConfig;
-    brandConfig: BrandConfig;
-    className?: string;
+  data: Data;
+  commonConfig: CommonConfig;
+  brandConfig: BrandConfig;
+  className?: string;
 }
